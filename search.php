df5b8996effca88eacc20a7b04de3fc8dc9ca68f
triabagus <triatop9@gmail.com>
2021-04-09 10:36:40 +0700

    fix show post with search

--- search.php
+++ search.php
@@ -4,8 +4,42 @@
 
 <main role="main" class="container">
   <div class="row">
-    <div class="col blog-main text-center">  
-        <?php get_search_form(); ?>
+    <div class="col blog-main text-center"> 
+      <?php if ( have_posts() ) : ?>	                         
+        <h1 class="page-title">
+          <?php	                        
+              /* translators: Search query.*/                        
+              printf( __( 'Search Results for: %s', 'bootstrapthemewordpress' ), '<span>' . get_search_query() . '</span>' );
+          ?>
+        </h1>
+      <?php else:  ?>
+        <h1 class="page-title"><?php _e( 'Nothing Found', 'bootstrapthemewordpress' ); ?></h1>
+      <?php endif; ?> 
+
+      <?php
+	    if ( have_posts() ) :
+        // Start the Loop.
+        while ( have_posts() ) :
+          the_post(); 
+          /**
+           * Run the loop for the search to output the results.
+           * If you want to overload this in a child theme then include a file
+           * called content-search.php and that will be used instead.                            
+           * */
+          get_template_part( 'template-parts/content', get_post_format() );
+        endwhile; // End the loop.
+	
+        //pagination
+	
+      else :
+      ?>
+          <p>
+            <?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'bootstrapthemewordpress' ); ?></p>
+      <?php
+          get_search_form();
+      endif;
+      ?>
+      
     </div><!-- /.blog-main --> 
 
   </div><!-- /.row -->
@@ -14,3 +48,4 @@
 <?php
     get_footer();
 ?>
+
